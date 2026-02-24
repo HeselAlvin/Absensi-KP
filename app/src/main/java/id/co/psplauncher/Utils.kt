f52aa0b3a318e90aeb42c59b92f8d7cbf1623a2c
Hesel <Heselalvinpriyanto@gmail.com>
2026-02-24 14:14:30 +0700

    implement Utils class for reusable code

--- app/src/main/java/id/co/psplauncher/Utils.kt
+++ app/src/main/java/id/co/psplauncher/Utils.kt
@@ -20,11 +20,11 @@ object Utils {
             failure.isNetworkError -> {
                 view.snackbar("Gagal koneksi. Silahkan check kembali koneksi jaringan anda")
             }
+
             failure.errorCode == 401 -> {
-                // Tambahkan logika pindah ke LoginActivity jika session habis
             }
+
             else -> {
-                // Kita ambil error mentah dari server supaya kamu tahu apa yang kurang (Misal: kurang user_id)
                 val error = failure.errorBody?.string() ?: "Terjadi kesalahan sistem"
                 view.snackbar(error)
             }
@@ -34,19 +34,15 @@ object Utils {
     fun View.snackbar(message: String) {
         val snackbar = Snackbar.make(this, message, Snackbar.LENGTH_LONG)
         val sView = snackbar.view // Inisialisasi view snackbar
-
-        // Casting ke FrameLayout agar bisa mengatur margin dan posisi
         val layoutParams = sView.layoutParams as FrameLayout.LayoutParams
-
-        // SOLUSI ANTI MERAH: Cari ID snackbar_text menggunakan string nama paket
-        val snackId = context.resources.getIdentifier("snackbar_text", "id", "com.google.android.material")
+        val snackId =
+            context.resources.getIdentifier("snackbar_text", "id", "com.google.android.material")
         val snackTextView = sView.findViewById<TextView>(snackId)
-
         layoutParams.setMargins(10, 10, 10, 10)
         layoutParams.gravity = Gravity.BOTTOM
 
         sView.setPadding(10, 10, 10, 10)
-        snackTextView?.maxLines = 5 // Supaya pesan error panjang dari server kelihatan semua
+        snackTextView?.maxLines = 5
         sView.layoutParams = layoutParams
         snackbar.animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
         snackbar.show()
